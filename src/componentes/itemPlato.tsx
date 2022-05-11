293dd0db1691e879f4eeb01fabee57fceb62824f
Federico Tobar <tobar.federico@gmail.com>
2022-05-11 18:37:24 -0300

    subiendo front de sabor

---
+++ src/componentes/itemPlato.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { Button, Card, Col, Container, Nav, Row } from 'react-bootstrap';
+import plato from './platos';
+
+type platoParams = {
+    id:number;
+    menu:string;
+    imagen:string;
+    precio:number;
+}
+export const ItemPlato = (args: platoParams) => {
+
+    return(
+        <>
+            <Card style={{width: '18rem'}} className='margenesTarjeta'>
+                <Card.Img variant="top" className='maxAltoImg' src={"http://localhost:3000/images/"+args.imagen.toLowerCase()}></Card.Img>
+                <Card.Body className='tarjPpal'>
+                    <></>
+                    <Card.Title>{args.menu}</Card.Title>
+                    <Card.Text>
+                        PRECIO = ${args.precio}
+                    </Card.Text>
+                    
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
