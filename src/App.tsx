aed0b6fdb70e16d1b11aab4fbbe816fd3ddcf0c2
Diego Noblega <diegomendoza20015@gmail.com>
2022-06-09 13:56:49 -0300

    arregledao a grilla y ahora permite agregar articulo, falta modificar y eliminar

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import {Home} from './componentes/Home';
 import './App.css';
 import { GrillaArticulos } from './componentes/Grilla';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Formulario} from "./componentes/Formulario";
 
 class App extends Component{
   render(){
     return (
       <Routes>
         <Route path="/" element={<Home/>}/>
-            <Route path="/detalle">
-            <Route path="/grilla" element={<GrillaArticulos/>}/>
-            </Route>
+          <Route path="/detalle">
+          </Route>
+          <Route path="/grilla" element={<GrillaArticulos/>}/>
+          <Route path="/formulario/:id" element={<Formulario/>}>
+          </Route>
             <Route path="*" element={<Home/>}/>
       </Routes>
     );
